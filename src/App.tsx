e5315b3a599fec088349b5a13585ed1e0ad090f2
XaulimMatadordePorco244 <dmichaeldouglas00@gmail.com>
2025-09-15 08:28:52 -0400

    configurando variáveis de ambiente para o backend

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+
+import '.App.css'
+import { useEffect, useState } from 'react'
+
+type ProdutoType = {
+  _id: string,
+  nome: string,
+  preco: number,
+  urlfoto: string,
+  descricao: string
+}
+
+export default function App() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  useEffect(() => {
+    fetch('api/produtos')
+      .then((response) => response.json())
+      .then((data) => setProdutos(data))
+  }, [])
+  return (
+    <>
+      <div>MICHAEL</div>
+      {
+        produtos.map((produto) => (
+          <div> key={produto._id}
+            <h2>{produto.nome}</h2>
+            <p>{produto.descricao}</p>
+            <p>{produto.preco}</p>
+            <img src="{produto.urlfoto}" alt="{produto.nome}" width="200" /></div>
+        ))
+      }
+    </>
+  )
+}

e121d47768523fc43309d274285222e1e39e924f
XaulimMatadordePorco244 <dmichaeldouglas00@gmail.com>
2025-09-17 11:52:27 -0400

    criação da function handleForm

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,43 @@ export default function App() {
       .then((response) => response.json())
       .then((data) => setProdutos(data))
   }, [])
+
+
+  function handleForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+
+    fetch('api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nome: formData.get('nome'),
+      })
+    })
+      .then((response) => response.json())
+      .then((newProduto) => {
+        setProdutos((prevProdutos) => [...prevProdutos, newProduto])
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      })
+    form.reset()
+  }
+
   return (
     <>
-      <div>MICHAEL</div>
+      <div>Cadastro de Produtos</div>
+      <form onSubmit={handleForm}>
+        <input type="text" name="nome" placeholder="Nome" required />
+        <input type="number" name="preco" placeholder="Preço" required />
+        <input type="text" name="urlfoto" placeholder="URL da Foto" required />
+        <textarea name="descricao" placeholder="Descrição" required></textarea>
+        <button type="submit">Cadastrar</button>
+      </form>
+      <div>LISTA DE PRODUTOS</div>
       {
         produtos.map((produto) => (
           <div key={produto._id}>
